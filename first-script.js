612456822f0d5d77b00deac6f64833223ef54870
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-06-07 10:34:47 +0200

    init project

---
+++ first-script.js
@@ -0,0 +1,6 @@
+"use strict";
+var age;
+age = 30;
+age = '30';
+console.log(age);
+//# sourceMappingURL=first-script.js.map
---
+++ first-script.ts
@@ -0,0 +1,6 @@
+
+let age:Number|string;
+age = 30 ;
+age = '30';
+
+console.log(age);
